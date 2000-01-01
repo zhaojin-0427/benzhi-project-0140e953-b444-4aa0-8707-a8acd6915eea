2c7b37684105dbaccc31d50754ffab41bad7c7f6
Benzhi Claimant <claimant@localhost>
2000-01-01 00:00:00 +0000

    Client result for 0140e953-b444-4aa0-8707-a8acd6915eea-bug-5

--- internal/application/queries.go
+++ internal/application/queries.go
@@ -4,7 +4,7 @@ import "specimen-custody-gate/internal/domain"
 
 type readinessCacheEntry struct {
 	version int
-	report  *domain.DepartureReadinessReport
+	report  domain.DepartureReadinessReport
 }
 
 func (s *Service) DepartureReadiness(batchID string) (*domain.DepartureReadinessReport, error) {
@@ -15,9 +15,23 @@ func (s *Service) DepartureReadiness(batchID string) (*domain.DepartureReadiness
 	s.mu.Lock()
 	defer s.mu.Unlock()
 	if cached, ok := s.readiness[batchID]; ok && cached.version == batch.Version {
-		return cached.report, nil
+		report := cloneDepartureReadinessReport(cached.report)
+		return &report, nil
 	}
-	report := batch.DepartureReadiness()
-	s.readiness[batchID] = readinessCacheEntry{version: batch.Version, report: &report}
-	return &report, nil
+	computed := batch.DepartureReadiness()
+	s.readiness[batchID] = readinessCacheEntry{version: batch.Version, report: cloneDepartureReadinessReport(computed)}
+	return &computed, nil
+}
+
+// cloneDepartureReadinessReport returns a copy of report whose Issues slice
+// does not share backing storage with the input, so callers mutating the
+// returned Issues cannot corrupt the cached value or vice versa.
+func cloneDepartureReadinessReport(report domain.DepartureReadinessReport) domain.DepartureReadinessReport {
+	clone := report
+	if report.Issues != nil {
+		issues := make([]domain.ValidationIssue, len(report.Issues))
+		copy(issues, report.Issues)
+		clone.Issues = issues
+	}
+	return clone
 }
